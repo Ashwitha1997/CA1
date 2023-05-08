d34a0edc0278e92f041409b534993ea414761334
unknown <l00171367@atu.ie>
2023-05-08 02:27:52 +0100

    Initial commit

---
+++ Q2.R
@@ -0,0 +1,88 @@
+#Q2
+
+#Using pairs() to examine correlations between variables
+pairs(heart_data, labels = colnames(heart_data), main = "Heart failure dataset correlation plot")
+
+
+# We can use libraries to help improve 
+# the chart. Also includes correlations between variables
+install.packages("psych")
+library(psych)
+
+pairs.panels(heart_data,
+             smooth = TRUE,      # If TRUE, draws  smooths
+             scale = FALSE,      # If TRUE, scales the correlation text font
+             density = TRUE,     # If TRUE, adds density plots and histograms
+             ellipses = TRUE,    # If TRUE, draws ellipses
+             method = "spearman",# Correlation method (also "pearson" or "kendall")
+             pch = 21,           # pch symbol
+             lm = FALSE,         # If TRUE, plots linear fit rather than the LOESS (smoothed) fit
+             cor = TRUE,         # If TRUE, reports correlations
+             jiggle = FALSE,     # If TRUE, data points are jittered
+             factor = 2,         # Jittering factor
+             hist.col = 4,       # Histograms color
+             stars = TRUE,       # If TRUE, adds significance level with stars
+             ci = TRUE)          # If TRUE, adds confidence intervals
+
+#showing the categorical data in a plot
+attach(heart_data)
+plot(age, DEATH_EVENT, pch=19, col= "lightblue")
+
+install.packages("ggplot2")
+library(ggplot2)
+ggplot(heart_data, aes(x= DEATH_EVENT, y= age)) + geom_boxplot()
+
+library("lattice")
+attach(heart_data)
+histogram(age ,DEATH_EVENT, 
+          data = heart_data, 
+          main = "Distribution of heart failure data", 
+          xlab = "Patients age", 
+          ylab = "Risk of heart failure")
+detach(heart_data)
+
+
+#-----------------------------------------------------------------
+#Appropriate testing 
+#-----------------------------------------------------------------
+# Quantile-quantile plot allows us to check if the
+# data is distributed normally
+attach(heart_data)
+qqnorm(age)
+
+#line represents normal distribution
+qqline(age, col = "red")
+
+opar <- par(no.readonly = TRUE)
+par(mfrow = c(1, 2))
+
+with(heart_data, {
+  qqnorm(age[DEATH_EVENT == "yes"], 
+         main = "Risk of heart failure depending on Age")
+  qqline(age[DEATH_EVENT == "yes"])
+})
+
+with(heart_data, {
+  qqnorm(age[DEATH_EVENT == "no"], 
+         main = "Risk of heart failure not depending on Age")
+  qqline(age[DEATH_EVENT == "no"])
+})
+
+par(opar)
+
+# Formal test of normality
+# provided through widely used Shapiro-Wilks test
+normality_test <- shapiro.test(heart_data$age)
+normality_test$p.value
+# p-value tells us the chances that the sample comes 
+# from a normal distribution 
+# p-value is clearly lower than 0.05
+# so not normally distributed
+
+
+#chi-squared test
+attach(heart_data)
+table <- table(age, DEATH_EVENT)
+result <- chisq.test(table)
+result
+detach(heart_data)
